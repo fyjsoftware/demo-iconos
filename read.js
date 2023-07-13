e3a3bbd6d04a6033a772fc75de5893722e230ebb
ItsJorgeRdz03 <82615294+ItsJorgeRdz03@users.noreply.github.com>
2023-07-13 15:38:51 -0700

    Codigo JS Lee Excel

--- read.js
+++ read.js
@@ -1,39 +1,47 @@
-const XLSX = require("xlsx");
+import XLSX from "xlsx";
+import * as db from "./db-test.js";
 
 /*const wb = XLSX.readFile(
   "Clockify_Time_Report_Detailed_06_01_2023-06_30_2023_1.xlsx"
 );
 XLSX.writeFile(wb, "compressed.xlsx", { compression: true });*/ // Compresion del Archivo Excel
 
-const wb1 = XLSX.readFile("compressed.xlsx"); // Archivo a leer
+const wb1 = await XLSX.readFile("compressed.xlsx"); // Archivo a leer
 const ws = wb1.Sheets["Detailed Report"]; // Hoja del archivo a leer
 
-var proyecto;
+var proyecto; // Falta Descripcion
 var cliente;
 var usuario;
 var tags;
 var fechaIn;
+var fechaFin;
 var horaIn;
+var horaFin;
 var duracion;
 
 function newArrVal() {
+  // Falta Descripcion
   proyecto = [];
   cliente = [];
   usuario = [];
   tags = [];
   fechaIn = [];
+  fechaFin = [];
   horaIn = [];
+  horaFin = [];
   duracion = [];
 }
 
 function getData(index) {
-  // Manda los datos que estan en el indice a los arreglos
+  // Manda los datos que estan en el indice a los arreglos | Falta descripcion
   proyecto.push(ws[`A${index}`].v);
   cliente.push(ws[`B${index}`].v);
   usuario.push(ws[`E${index}`].v);
   tags.push(ws[`H${index}`].v);
   fechaIn.push(ws[`J${index}`].v);
+  fechaFin.push(ws[`L${index}`].v);
   horaIn.push(ws[`K${index}`].v);
+  horaFin.push(ws[`M${index}`].v);
   duracion.push(ws[`N${index}`].v);
 }
 
@@ -43,21 +51,22 @@ function simpleSearch(min, max) {
   for (let index = min; index < max; index++) {
     getData(index);
   }
-  printData(proyecto, cliente, usuario, tags, fechaIn, horaIn, duracion);
+  sendData(
+    proyecto,
+    cliente,
+    usuario,
+    tags,
+    fechaIn,
+    fechaFin,
+    horaIn,
+    horaFin,
+    duracion
+  );
 }
 
-function getClientProj(client) {
+async function getClientProj(client) {
   //Obtiene todas las tuplas de un cliente especifico
-  newArrVal();
-  var index = 2;
-  const max = getCurrentIndex();
-  while (index < max) {
-    if (client == ws[`B${index}`].v) {
-      getData(index);
-    }
-    index++;
-  }
-  printData(proyecto, cliente, usuario, tags, fechaIn, horaIn, duracion);
+  db.getRow(client);
 }
 
 function getCurrentIndex() {
@@ -69,9 +78,6 @@ function getCurrentIndex() {
   var a2;
   var a3;
   while (indexVal > 1) {
-    /*a1 = ws[`J${index}`]?.v;
-    a2 = ws[`K${index}`]?.v;
-    a3 = ws[`L${index}`]?.v;*/
     if (
       ws[`J${index}`]?.v != undefined ||
       ws[`K${index}`]?.v != undefined ||
@@ -88,7 +94,71 @@ function getCurrentIndex() {
   return index;
 }
 
+function sendData(...args) {
+  // Funcion de prueba para comunicarse con la base de datos
+  let index = 0;
+  for (let index = 0; index < proyecto.length; index++) {
+    db.addRow(
+      proyecto[index],
+      cliente[index],
+      usuario[index],
+      tags[index],
+      parseDate(fechaIn[index]),
+      parseDate(fechaFin[index]),
+      parseTime(horaIn[index]),
+      parseTime(horaFin[index]),
+      duracion[index]
+    );
+  }
+}
+
+function parseDate(fecha) {
+  //Funcion que convierte la fecha en un formato legible de la base de datos
+  let day = "";
+  let month = "";
+  let year = "";
+  for (let i = 0; i < fecha.length; i++) {
+    if (i < 2) {
+      month += fecha.charAt(i);
+    } else if (i > 2 && i < 5) {
+      day += fecha.charAt(i);
+    } else if (i > 5) {
+      year += fecha.charAt(i);
+    }
+  }
+  fecha = `${year}-${month}-${day}`;
+  return fecha;
+}
+
+function parseTime(hora) {
+  //Funcion que convierte la hora en un formato legible de la base de datos
+  let hour = "";
+  let min = "";
+  let period = "";
+  let st = true;
+  for (let i = hora.length - 1; i >= 0; i--) {
+    if (i > hora.length - 3) {
+      period += hora.charAt(i);
+    } else if (i < hora.length - 3 && i > hora.length - 6) {
+      min += hora.charAt(i);
+    } else if (i < hora.length - 6) {
+      hour += hora.charAt(i);
+    }
+  }
+  hour = hour.charAt(1) + hour.charAt(0);
+  min = min.charAt(1) + min.charAt(0);
+  if (period.charAt(1) == `P`) {
+    hour = parseInt(hour, 10) + 12;
+  }
+  hora = `${hour}:${min}`;
+  return hora;
+}
+
+//getClientProj("Winning Solutions INC");
+simpleSearch(2, getCurrentIndex());
+
 function printData(...args) {
+  // Funcion de prueba para leer el excel
   // Imprime la informacion dada
   let index = 0;
   for (let index = 0; index < proyecto.length; index++) {
@@ -104,6 +174,22 @@ function printData(...args) {
   }
 }
 
+/*function getClientProj(client) {
+  // Funcion de prueba para leer el excel
+  //Obtiene todas las tuplas de un cliente especifico
+  newArrVal();
+  var index = 2;
+  const max = getCurrentIndex();
+  while (index < max) {
+    if (client == ws[`B${index}`].v) {
+      getData(index);
+    }
+    index++;
+  }
+  printData(proyecto, cliente, usuario, tags, fechaIn, horaIn, duracion);
+}*/
+
 //getCurrentIndex();
-//simpleSearch(2, 12);
-getClientProj("Winning Solutions INC");
+//await simpleSearch(2, 20);
+//await db.getRow();
+await db.stop();
