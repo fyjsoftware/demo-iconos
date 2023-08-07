34feaa1fe41ba45947864822fd24a2fd143f1fd5
ItsJorgeRdz03 <82615294+ItsJorgeRdz03@users.noreply.github.com>
2023-08-07 14:54:06 -0700

    Agregado tabla1

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const config = JSON.parse(
-	await fs.readFile(new URL("./config.json", import.meta.url))
+  await fs.readFile(new URL("./config.json", import.meta.url))
 );
 
 const app = express();
@@ -25,150 +25,191 @@ app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
 
 var storage = await multer.diskStorage({
-	destination: async function (req, file, callback) {
-		callback(null, __dirname + "/temp");
-	},
-	filename: async function (req, file, callback) {
-		callback(null, file.originalname);
-	},
+  destination: async function (req, file, callback) {
+    callback(null, __dirname + "/temp");
+  },
+  filename: async function (req, file, callback) {
+    callback(null, file.originalname);
+  },
 });
 var upload = await multer({ storage: storage }).single("archivo");
 
 export async function iniciar() {
-	app.get("/", async function (request, response) {
-		await response.sendFile(__dirname + "/static/upload.html");
-	});
+  app.get("/", async function (request, response) {
+    await response.sendFile(__dirname + "/static/upload.html");
+  });
 
-	app.get("/:tabla", async function (request, response) {
-		try {
-			try {
-				await fs.stat(__dirname + "/temp/" + request.params.tabla + "_loading");
-				await response.render("warning", {
-					texto:
-						"El reporte ha sido cargado al sistema. Por favor, espere unos minutos a que se termine de subir y revise nuevamente.<br><br><b>Progreso de subida</b>: " +
-						(await fs.readFile(
-							new URL(
-								"./temp/" + request.params.tabla + "_loading",
-								import.meta.url
-							)
-						)),
-				});
-			} catch (error) {
-				if ((error.code = "ENOENT")) {
-                    let dev = request.query.desarrollador !== undefined ? request.query.desarrollador : null;
-                    let cliente = request.query.cliente !== undefined ? request.query.cliente : null;
-					await response.render("data", {
-                        tabla: request.params.tabla,
-                        listas: await db.mostrarListas(request.params.tabla),
-						resultado: await db.mostrar(request.params.tabla, { dev: dev, cliente: cliente })
-					});
-				} else {
-					await response.render("error", {
-						error: error,
-					});
-				}
-			}
-		} catch (error) {
-			if (error.stack.includes("doesn't exist")) {
-				await response.render("warning", {
-					texto:
-						"El reporte al que intenta acceder no existe. Por favor, verifique que haya escrito el enlace correctamente.",
-				});
-			} else {
-				await logger.error(error.stack);
-				await response.render("error", {
-					error: error,
-				});
-			}
-		}
-	});
+  app.get("/:tabla", async function (request, response) {
+    try {
+      try {
+        await fs.stat(__dirname + "/temp/" + request.params.tabla + "_loading");
+        await response.render("warning", {
+          texto:
+            "El reporte ha sido cargado al sistema. Por favor, espere unos minutos a que se termine de subir y revise nuevamente.<br><br><b>Progreso de subida</b>: " +
+            (await fs.readFile(
+              new URL(
+                "./temp/" + request.params.tabla + "_loading",
+                import.meta.url
+              )
+            )),
+        });
+      } catch (error) {
+        if ((error.code = "ENOENT")) {
+          let dev =
+            request.query.desarrollador !== undefined
+              ? request.query.desarrollador
+              : null;
+          let cliente =
+            request.query.cliente !== undefined ? request.query.cliente : null;
+          await response.render("data", {
+            tabla: request.params.tabla,
+            listas: await db.mostrarListas(request.params.tabla),
+            resultado: await db.mostrar(
+              request.params.tabla,
+              { dev: dev, cliente: cliente },
+              false
+            ),
+          });
+        } else {
+          await response.render("error", {
+            error: error,
+          });
+        }
+      }
+    } catch (error) {
+      if (error.stack.includes("doesn't exist")) {
+        await response.render("warning", {
+          texto:
+            "El reporte al que intenta acceder no existe. Por favor, verifique que haya escrito el enlace correctamente.",
+        });
+      } else {
+        await logger.error(error.stack);
+        await response.render("error", {
+          error: error,
+        });
+      }
+    }
+  });
 
-	app.get("/delete/:tabla", async function (request, response) {
-		try {
-			await db.eliminar(request.params.tabla);
-			await response.redirect("/");
-		} catch (error) {
-			await logger.error(error.stack);
-			await response.render("error", {
-				error: error,
-			});
-		}
-	});
+  // No se si el siguiente metodo sea lo correcto para mostrar la tabla de la duracion total
+  app.get("/:tabla1", async function (request, response) {
+    try {
+      let dev =
+        request.query.desarrollador !== undefined
+          ? request.query.desarrollador
+          : null;
+      let cliente =
+        request.query.cliente !== undefined ? request.query.cliente : null;
+      await response.render("data1", {
+        tabla: request.params.tabla1,
+        listas: await db.mostrarListas(request.params.tabla1),
+        resultado: await db.mostrar(
+          request.params.tabla1,
+          { dev: dev, cliente: cliente },
+          true
+        ),
+      });
+    } catch (error) {
+      if (error.stack.includes("doesn't exist")) {
+        await response.render("warning", {
+          texto:
+            "El reporte al que intenta acceder no existe. Por favor, verifique que haya escrito el enlace correctamente.",
+        });
+      } else {
+        await logger.error(error.stack);
+        await response.render("error", {
+          error: error,
+        });
+      }
+    }
+  });
 
-	app.post("/upload", async function (request, response) {
-		try {
-			await upload(request, response, async function (error) {
-				if (error) {
-					throw error;
-				}
-				if (!request.file.originalname.endsWith(".xlsx")) {
-					throw new Error(
-						"El archivo que subió no es un archivo formato Excel. Por favor, intente con otro archivo."
-					);
-				}
-				let codigo = await db.generarCodigo();
-				let temp = __dirname + "/temp/" + codigo;
-				let length = 0;
-				let errored = false;
+  app.get("/delete/:tabla", async function (request, response) {
+    try {
+      await db.eliminar(request.params.tabla);
+      await response.redirect("/");
+    } catch (error) {
+      await logger.error(error.stack);
+      await response.render("error", {
+        error: error,
+      });
+    }
+  });
 
-				try {
-					length = await excel.leer(codigo, request.file.originalname);
-					await fs.writeFile(temp + "_loading", "0.00%");
-					await fs.access(temp + ".json");
-					await response.redirect("/" + codigo);
-				} catch (error) {
-					errored = true;
-					if (!response.headersSent) {
-						await logger.error(error.stack);
-						await response.render("error", {
-							error: error,
-						});
-					}
-				}
+  app.post("/upload", async function (request, response) {
+    try {
+      await upload(request, response, async function (error) {
+        if (error) {
+          throw error;
+        }
+        if (!request.file.originalname.endsWith(".xlsx")) {
+          throw new Error(
+            "El archivo que subió no es un archivo formato Excel. Por favor, intente con otro archivo."
+          );
+        }
+        let codigo = await db.generarCodigo();
+        let temp = __dirname + "/temp/" + codigo;
+        let length = 0;
+        let errored = false;
 
-				try {
-					if (!errored)
-						await db.crear(
-							codigo,
-							await excel.codificar(temp + ".json", length)
-						);
-				} catch (error) {
-					if (!response.headersSent) {
-						await logger.error(error.stack);
-						await response.render("error", {
-							error: error,
-						});
-					}
-				}
+        try {
+          length = await excel.leer(codigo, request.file.originalname);
+          await fs.writeFile(temp + "_loading", "0.00%");
+          await fs.access(temp + ".json");
+          await response.redirect("/" + codigo);
+        } catch (error) {
+          errored = true;
+          if (!response.headersSent) {
+            await logger.error(error.stack);
+            await response.render("error", {
+              error: error,
+            });
+          }
+        }
 
-				await limpiarTemp();
-			});
-		} catch (error) {
-			await logger.error(error.stack);
-			if (!response.headersSent) {
-				await response.render("error", {
-					error: error,
-				});
-			}
-			await limpiarTemp();
-		}
-	});
+        try {
+          if (!errored)
+            await db.crear(
+              codigo,
+              await excel.codificar(temp + ".json", length)
+            );
+        } catch (error) {
+          if (!response.headersSent) {
+            await logger.error(error.stack);
+            await response.render("error", {
+              error: error,
+            });
+          }
+        }
 
-	app.listen(config.puertos.http, config.host, async function () {
-		await logger.info(
-			"Servidor web iniciando en " + config.host + ":" + config.puertos.http
-		);
-	});
+        await limpiarTemp();
+      });
+    } catch (error) {
+      await logger.error(error.stack);
+      if (!response.headersSent) {
+        await response.render("error", {
+          error: error,
+        });
+      }
+      await limpiarTemp();
+    }
+  });
+
+  app.listen(config.puertos.http, config.host, async function () {
+    await logger.info(
+      "Servidor web iniciando en " + config.host + ":" + config.puertos.http
+    );
+  });
 }
 
 app.on("error", (err) => {
-	throw err;
+  throw err;
 });
 
 async function limpiarTemp() {
-	const archivos = await fs.readdir(__dirname + "/temp");
-	for (const archivo of archivos) {
-		await fs.unlink(path.resolve(__dirname + "/temp", archivo));
-	}
-	await logger.info("Limpieza de la carpeta temporal completa");
+  const archivos = await fs.readdir(__dirname + "/temp");
+  for (const archivo of archivos) {
+    await fs.unlink(path.resolve(__dirname + "/temp", archivo));
+  }
+  await logger.info("Limpieza de la carpeta temporal completa");
 }
