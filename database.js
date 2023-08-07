d98a125ae793a5117afd0fdab6bc1315de11409d
ItsJorgeRdz03 <82615294+ItsJorgeRdz03@users.noreply.github.com>
2023-08-07 14:52:58 -0700

    Modificación consultas

--- database.js
+++ database.js
@@ -5,67 +5,96 @@ import { fileURLToPath } from "url";
 import path from "path";
 
 const config = JSON.parse(
-	await fs.readFile(new URL("./config.json", import.meta.url))
+  await fs.readFile(new URL("./config.json", import.meta.url))
 );
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 async function conexion() {
-    return await mysql.createConnection({
-        host: config.testing ? config.hostSecundario : config.host,
-        port: config.puertos.db,
-        user: config.db.usuario,
-        password: config.db.password,
-        database: config.db.baseDatos
-    });;
+  return await mysql.createConnection({
+    host: config.testing ? config.hostSecundario : config.host,
+    port: config.puertos.db,
+    user: config.db.usuario,
+    password: config.db.password,
+    database: config.db.baseDatos,
+  });
 }
 
-export async function mostrar(tabla, query) {
-    let filtro = "";
-    if (query.dev !== null && query.cliente === null) {
-        filtro = " WHERE desarrollador = \"" + query.dev + "\"";
+/* Agregué un último argumento booleano el cual dependiendo si es verdadero o falso da la duracion total de los devs
+   También modifiqué ligeramente las consultas y dentro de estas use LIKE*/
+export async function mostrar(tabla, query, st) {
+  let filtro = "";
+  if (query.dev !== null && query.cliente === null) {
+    filtro = ' WHERE desarrollador LIKE "%' + query.dev + '%"';
+  }
+  if (query.cliente !== null && query.dev === null) {
+    filtro = ' WHERE cliente LIKE "%' + query.cliente + '%"';
+  }
+  if (query.dev !== null && query.cliente !== null) {
+    filtro =
+      ' WHERE desarrollador LIKE "%' +
+      query.dev +
+      '%" AND cliente LIKE "%' +
+      query.cliente +
+      '%"';
+  }
+  let database = await conexion();
+  let newR = null;
+  if (st == false) {
+    const [resultados] = await database.execute(
+      "SELECT desarrollador, cliente, proyecto, tags, fechaInicio, duracion from " +
+        tabla +
+        filtro +
+        ";"
+    );
+    newR = resultados;
+  } else {
+    const [resultados] = await database.execute(
+      "SELECT desarrollador, cliente, proyecto, SUM(duracion) AS duracion from " +
+        tabla +
+        filtro +
+        " GROUP BY desarrollador, cliente, proyecto;"
+    );
+    newR = resultados;
+  }
+  await database.end();
+  if (st == false) {
+    for (let i = 0; i < newR.length; i++) {
+      newR[i].tags =
+        newR[i].tags === null ? "" : newR[i].tags.replaceAll(",", "<br>");
+      newR[i].fechaInicio =
+        newR[i].fechaInicio === null
+          ? ""
+          : newR[i].fechaInicio.toLocaleDateString("en-US");
     }
-    if (query.cliente !== null && query.dev === null) {
-        filtro = " WHERE cliente = \"" + query.cliente + "\"";
-    }
-    if (query.dev !== null && query.cliente !== null) {
-        filtro = " WHERE desarrollador = \"" + query.dev + "\" AND cliente = \"" + query.cliente + "\"";
-    }
-    let database = await conexion();
-	const [resultados] = await database.execute("SELECT * from " + tabla + filtro + ";");
-	let newR = resultados;
-    await database.end();
-	for (let i = 0; i < newR.length; i++) {
-		newR[i].tags =
-			newR[i].tags === null ? "" : newR[i].tags.replaceAll(",", "<br>");
-		newR[i].fechaInicio =
-			newR[i].fechaInicio === null
-				? ""
-				: newR[i].fechaInicio.toLocaleDateString("en-US");
-	}
-	return newR;
+  }
+  return newR;
 }
 
 export async function mostrarListas(tabla) {
-    let database = await conexion();
-    const [devs] = await database.execute("SELECT DISTINCT desarrollador FROM " + tabla + ";");
-    const [clientes] = await database.execute("SELECT DISTINCT cliente FROM " + tabla + ";");
-    await database.end();
-    let arr1 = [];
-    let arr2 = [];
-    for (let dev of devs) {
-        if (dev !== "") arr1.push(dev.desarrollador);
-    }
-    for (let cliente of clientes) {
-        if (cliente !== "") arr2.push(cliente.cliente);
-    }
-	return { devs: JSON.stringify(arr1), clientes: JSON.stringify(arr2) };
+  let database = await conexion();
+  const [devs] = await database.execute(
+    "SELECT DISTINCT desarrollador FROM " + tabla + ";"
+  );
+  const [clientes] = await database.execute(
+    "SELECT DISTINCT cliente FROM " + tabla + ";"
+  );
+  await database.end();
+  let arr1 = [];
+  let arr2 = [];
+  for (let dev of devs) {
+    if (dev !== "") arr1.push(dev.desarrollador);
+  }
+  for (let cliente of clientes) {
+    if (cliente !== "") arr2.push(cliente.cliente);
+  }
+  return { devs: JSON.stringify(arr1), clientes: JSON.stringify(arr2) };
 }
 
 export async function crear(tabla, json) {
-    let database = await conexion();
-	await database.execute(`
+  let database = await conexion();
+  await database.execute(`
     CREATE TABLE IF NOT EXISTS \`sistema\`.\`${tabla}\` (
         \`idReporte\` SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
         \`desarrollador\` VARCHAR(50) NULL DEFAULT NULL,
@@ -85,35 +114,35 @@ export async function crear(tabla, json) {
       COLLATE = utf8mb4_0900_ai_ci
       COMMENT = 'Tabla generada automáticamente por el Proyecto ICONOS.';
       `);
-	logger.info(
-		"Tabla creada en base de datos: " +
-			tabla +
-			", datos a agregar: " +
-			json.length
-	);
-	for (let i = 0; i < json.length; i++) {
-		await database.execute(`INSERT INTO ${tabla} (idReporte, desarrollador,
+  logger.info(
+    "Tabla creada en base de datos: " +
+      tabla +
+      ", datos a agregar: " +
+      json.length
+  );
+  for (let i = 0; i < json.length; i++) {
+    await database.execute(`INSERT INTO ${tabla} (idReporte, desarrollador,
             cliente, proyecto, tags, fechaInicio, duracion)
         VALUES (NULL, ${json[i].desarrollador}, ${json[i].cliente},
             ${json[i].proyecto}, ${json[i].tags}, ${json[i].fechaInicio},
             ${json[i].duracion});`);
-		await fs.writeFile(
-			__dirname + "/temp/" + tabla + "_loading",
-			parseFloat((i / json.length) * 100).toFixed(2) + "%"
-		);
-	}
-    await database.end();
-	logger.info("Adición de datos a la tabla " + tabla + " terminada");
+    await fs.writeFile(
+      __dirname + "/temp/" + tabla + "_loading",
+      parseFloat((i / json.length) * 100).toFixed(2) + "%"
+    );
+  }
+  await database.end();
+  logger.info("Adición de datos a la tabla " + tabla + " terminada");
 }
 
 export async function eliminar(tabla) {
-    let database = await conexion();
-	await database.execute(`DROP TABLE \`sistema\`.\`${tabla}\`;`);
-    await database.end();
-	logger.info("Tabla eliminada: " + tabla);
+  let database = await conexion();
+  await database.execute(`DROP TABLE \`sistema\`.\`${tabla}\`;`);
+  await database.end();
+  logger.info("Tabla eliminada: " + tabla);
 }
 
 export async function generarCodigo() {
-	let temp = Math.random().toString(36).slice(2);
-	return temp.substring(temp.length - 5);
+  let temp = Math.random().toString(36).slice(2);
+  return temp.substring(temp.length - 5);
 }
